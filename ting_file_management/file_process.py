a4d38ae12e9c412cf9fe88c11aac44ebbc6c8bec
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-03-15 11:57:24 -0400

    [feat]: testando process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,27 @@
-def process(path_file, instance):
+import sys
+
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
+    """Aqui irá sua implementação"""
+    file_read = txt_importer(path_file)
+    add_obj = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_read),
+        "linhas_do_arquivo": file_read
+    }
+
+    for num in range(len(instance) - 1):
+        if instance.search(num)['nome_do_arquivo'] == path_file:
+            return None
+
+    instance.enqueue(add_obj)
+
+def remove(instance: Queue):
     """Aqui irá sua implementação"""
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
-
-
-def file_metadata(instance, position):
+def file_metadata(instance: Queue, position):
     """Aqui irá sua implementação"""
